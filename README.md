3337bcf4ba67b17a6ba39ba60b5529b4bfd84184
MrKhantee <mrkhantee@gmail.com>
2020-02-28 14:56:12 +0700

    Correct Error

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ listview_with_image_using_array_adapter
 This Android Tutorial video shows how to create an Android ListView with image that uses a custom ArrayAdapter to fetch data from string-array located inside strings.xml in res folder. Use the getView method of your Custom ArrayAdapter to inflate a layout file that contains an ImageView and 2 TextViews to fill the data in and pass it back to the ListView
 
 Create the drawable folder and add 10 images under it for this project to work. Each image preferably 48x48px and named meme1, meme2...10
+
+![Screenshot](Screenshot_2020-02-28-14-52-42.png)
+
+![Screenshot](Screenshot_2020-02-28-14-52-53.png)
--- app/src/main/java/slidenerd/vivz/MainActivity.java
+++ app/src/main/java/slidenerd/vivz/MainActivity.java
@@ -1,42 +1,38 @@
 package slidenerd.vivz;
 
-import android.content.Context;
-import android.content.res.Resources;
-import android.os.Bundle;
-import android.app.Activity;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.content.*;
+import android.content.res.*;
+import android.os.*;
+import android.support.v7.app.*;
+import android.view.*;
+import android.widget.*;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     ListView list;
     String[] memeTitles;
     String[] memeDescriptions;
-    int[] images = {R.drawable.meme1, R.drawable.meme2, R.drawable.meme3, R.drawable.meme4, R.drawable.meme5, R.drawable.meme6, R.drawable.meme7, R.drawable.meme8, R.drawable.meme9, R.drawable.meme10, R.drawable.meme1, R.drawable.meme2, R.drawable.meme3, R.drawable.meme4, R.drawable.meme5, R.drawable.meme6, R.drawable.meme7, R.drawable.meme8, R.drawable.meme9, R.drawable.meme10,R.drawable.meme1, R.drawable.meme2, R.drawable.meme3, R.drawable.meme4, R.drawable.meme5, R.drawable.meme6, R.drawable.meme7, R.drawable.meme8, R.drawable.meme9, R.drawable.meme10, R.drawable.meme1, R.drawable.meme2, R.drawable.meme3, R.drawable.meme4, R.drawable.meme5, R.drawable.meme6, R.drawable.meme7, R.drawable.meme8, R.drawable.meme9, R.drawable.meme10};
+    int[] images = {R.drawable.meme1, R.drawable.meme2, R.drawable.meme3, R.drawable.meme4, R.drawable.meme4};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Resources res = getResources();
+   		startUp();
+    }
+
+	private void startUp()
+	{
+		Resources res = getResources();
         memeTitles = res.getStringArray(R.array.titles);
         memeDescriptions = res.getStringArray(R.array.descriptions);
 
         list = (ListView) findViewById(R.id.listView);
         VivzAdapter adapter = new VivzAdapter(this, memeTitles, images, memeDescriptions);
         list.setAdapter(adapter);
-
-    }
-
-
+	}
+	
 }
 
 class VivzAdapter extends ArrayAdapter<String> {
@@ -53,6 +49,7 @@ class VivzAdapter extends ArrayAdapter<String> {
         this.titleArray = titles;
         this.descriptionArray = desc;
     }
+	
     public View getView(int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View row = inflater.inflate(R.layout.single_row, parent, false);
